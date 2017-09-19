dd25eaa9161da849f905063374f9766e796e5292
VVestin <wmmetzler01@gmail.com>
2017-09-18 21:34:18 -0600

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOp.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by vvestin on 9/18/17.
+ */
+
+public class TestOp extends OpMode {
+
+    private DcMotor motor;
+
+    public void init() {
+        motor = hardwareMap.dcMotor.get("motor");
+    }
+
+    public void loop() {
+        
+    }
+}
